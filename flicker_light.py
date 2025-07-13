f87419b436cd03a4c16bfe63e8ec20fd732b6393
mickeyd-358 <michdeft01@gmail.com>
2025-07-13 19:40:20 +1000

    add comments

--- flicker_light.py
+++ flicker_light.py
@@ -1,10 +1,10 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(17)
+led = LED(17)   # use gpio17 port
 
-while True:
-    led.on()
-    sleep(1)
+while True:  # repeat forever
+    led.on()  # light on
+    sleep(1)  # light off
     led.off()
     sleep(1)
