b45f37e95b3d2a4aa8edf36459b5efe758ac653a
mickeyd-358 <michdeft01@gmail.com>
2025-07-14 19:13:03 +1000

    add two buttons and 2 LED's alternating

--- flicker_light.py
+++ flicker_light.py
@@ -1,13 +1,21 @@
 from gpiozero import LED, Button
 from time import sleep
 
-led = LED(19)   # use gpio 19 port
-button = Button(16)
+led_red = LED(13)   # use gpio 19 port
+led_green = LED(19)
+button_red = Button(21)
+button_green = Button(16)
 
 while True:  # repeat forever
-    button.wait_for_press()
-    led.on()
-    sleep(0.2)
-    button.wait_for_press()
-    led.off()
-    sleep(0.2)
+    button_red.wait_for_press()
+    led_red.on()
+    sleep(0.6)
+    button_red.wait_for_press()
+    led_red.off()
+    sleep(0.6)
+    button_green.wait_for_press()
+    led_green.on()
+    sleep(0.6)
+    button_green.wait_for_press()
+    led_green.off()
+    sleep(0.6)
